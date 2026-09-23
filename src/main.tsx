c8d0ac84eed4fa7f33e6c1e3a3fe683ec5816530
ryfexpro <ryfexpro@gmail.com>
2026-09-23 14:33:27 +0300

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import {TransferGardaPage} from './Home';
+import {SeoRoute} from './components/SeoRoute';
+import './components/scroll-scrub.css'; import './site.css';
+const p=location.pathname.replace(/\/$/,'')||'/';
+const pages:Record<string,React.ReactNode>={
+'/transfer-aeroporto-verona-lago-di-garda':<SeoRoute airport='Aeroporto di Verona (VRN)' title='Transfer Aeroporto Verona Lago di Garda' intro='North Transfer offre transfer privati con autista NCC dall’Aeroporto di Verona al Lago di Garda, per Riva del Garda, Arco, Torbole, Limone, Malcesine, Garda, Bardolino, Lazise e Sirmione.' />,
+'/transfer-aeroporto-bergamo-lago-di-garda':<SeoRoute airport='Aeroporto di Bergamo Orio al Serio (BGY)' title='Transfer Aeroporto Bergamo Lago di Garda' intro='North Transfer offre transfer privati con autista NCC dall’Aeroporto di Bergamo Orio al Serio al Lago di Garda.' />,
+'/transfer-malpensa-lago-di-garda':<SeoRoute airport='Aeroporto di Milano Malpensa (MXP)' title='Transfer Malpensa Lago di Garda' intro='North Transfer collega Milano Malpensa al Lago di Garda con transfer privati NCC.' />,
+'/transfer-venezia-lago-di-garda':<SeoRoute airport='Aeroporto di Venezia Marco Polo (VCE)' title='Transfer Venezia Lago di Garda' intro='North Transfer offre collegamenti privati NCC dall’Aeroporto di Venezia Marco Polo al Lago di Garda.' />};
+const node=p==='/en'?<TransferGardaPage initialLang='en'/>:(pages[p]??<TransferGardaPage initialLang='it'/>); createRoot(document.getElementById('root')!).render(<React.StrictMode>{node}</React.StrictMode>);
